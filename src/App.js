467f52b98fe85be1fff620ba09ce8215fd1ea627
Riciere Marcone <85197053+ricichien@users.noreply.github.com>
2023-05-17 23:09:01 -0300

    committing changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
-import React from 'react';
+import React from "react";
+
+import FlipClockCountdown from "@leenguyen/react-flip-clock-countdown";
+
+import "@leenguyen/react-flip-clock-countdown/dist/index.css";
+
+import VideoBg from "../src/assets/video.mp4";
+
+import "./styles.css";
 
 const App = () => {
-  return <div>react app</div>;
+  return (
+    <section className="page">
+      {/* overlay */}
+      <div className="overlay"></div>
+      {/* video */}
+      <video src={VideoBg} autoPlay loop muted></video>
+      {/* counter */}
+      <div className="page__content">
+        <h1>Lauching Soon</h1>
+        <h3>
+          Leave your email and we'll let you know once the site goes live.
+        </h3>
+        {/* clock */}
+        <FlipClockCountdown
+          to={new Date().getTime() + 24 * 3600 * 1000 + 5000}
+          className="flip-clock"
+          labels={["DAYS", "HOURS", "MINUTES", "SECONDS"]}
+          duration={0.5}
+        />
+        {/* button */}
+        <button className="btn">Notify Me</button>
+      </div>
+    </section>
+  );
 };
 
 export default App;
